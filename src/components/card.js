44f8134fe793161f67e9705edf7e72b582fc61aa
Evghenia Gradeskova <egradeskova@edu.hse.ru>
2024-06-26 15:46:42 +0300

    Проект 7ПР после 1ревью

--- src/components/card.js
+++ src/components/card.js
@@ -13,28 +13,28 @@ export function createCard(cardInfo, openCard, cardDelete, handleLike, handleIma
   cardImage.alt = cardInfo.name;
   cardTitle.textContent = cardInfo.name;
   likeCounter.textContent = cardInfo.likes.length;
+  
+   // Проверка, лайкнул ли текущий пользователь эту карточку
+   if (cardInfo.likes.some(like => like._id === userId)) {
+    likeButton.classList.add('card__like-button_is-active');
+  }
 
   if (userId !== cardInfo.owner._id) {
     deleteButton.disabled = true;
     deleteButton.classList.add('visually-hidden');
-  } else{
+  } else {
     deleteButton.addEventListener('click', () => cardDelete(cardInfo._id, copyListItem));
   }
-    cardImage.addEventListener('click', function() {
+
+  cardImage.addEventListener('click', function() {
     openCard(cardInfo.name, cardInfo.link);
   });
 
-  likeButton.addEventListener('click', () => {
-    const likeMethod = likeButton.classList.contains('card__like-button_is-active') ? unlikeCard : likeCard;
-    likeMethod(cardInfo._id) 
-            .then((res) => {
-              handleLike(likeButton); 
-               likeCounter.textContent = res.likes.length;
-            })
-    .catch(err => console.log(err)); 
-  });
+  // Обработка лайка через колбэк handleLike
+  likeButton.addEventListener('click', () => handleLike(cardInfo._id, likeButton, likeCounter));
   cardImage.addEventListener("click", handleImageClick);
-return copyListItem;
+
+  return copyListItem;
 };
 
 // Функция удаления карточки
@@ -49,6 +49,17 @@ export function cardDelete (id, copyListItem) {
 };
 
 // Функция добавления лайка
-export function handleLike(likeButton) { 
-  likeButton.classList.toggle('card__like-button_is-active'); 
-}; 
+export function handleLike(cardId, likeButton, likeCounter) {
+  const likeMethod = likeButton.classList.contains('card__like-button_is-active') ? unlikeCard : likeCard;
+
+  likeMethod(cardId)
+    .then((res) => {
+      if (likeButton.classList.contains('card__like-button_is-active')) {
+        likeButton.classList.remove('card__like-button_is-active');
+      } else {
+        likeButton.classList.add('card__like-button_is-active');
+      }
+      likeCounter.textContent = res.likes.length;
+    })
+    .catch(err => console.log(err));
+};
